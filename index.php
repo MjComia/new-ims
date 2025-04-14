c2cbac7b04ffcbfb3549e475f1760737ac6b147f
MjComia <markjoel.comia@tup.edu.ph>
2025-04-14 16:35:43 +0800

    added files
    Started recreating new ims

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+include "header.php";
+include "navbar.php";
+?>
+
+
+
+
+
+<?php include "footer.php"; ?>
---
+++ navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-success bg-gradient mt-5 ">
+  <div class="container-fluid ">
+    <a class="navbar-brand text-light" href="#">Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="#">Customer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Transactions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Suppliers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Stock</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</nav>
