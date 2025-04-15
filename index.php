1ba69c0f99299475d6cf74bb3b9fee0c42042f2a
MjComia <markjoel.comia@tup.edu.ph>
2025-04-15 13:08:24 +0800

    Add initial HTML structure with DataTable integration
    - Created a new HTML file with a basic structure.
    - Included DataTables library for enhanced table functionality.
    - Added a sample table with two columns and two rows of data.
    - Integrated jQuery and DataTables scripts for dynamic table features.

--- index.php
+++ index.php
@@ -4,9 +4,36 @@
 include "header.php";
 include "navbar.php";
 ?>
+<!-- DataTables -->
+
+<table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
 
 
+<!-- jQuery -->
+<script src="lib/jquery/jquery-3.7.1.min.js"></script>
+<!-- DataTables -->
+<script src="lib/datatables/dataTables.js"></script>
 
-
-
+<script>
+  $(document).ready(function () {
+    $('#myTable').DataTable(); // Activate DataTable
+  });
+</script>
 <?php include "footer.php"; ?>
