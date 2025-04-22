f37544379a02e13d196b6c392f92092fc74b2abb
MjComia <markjoel.comia@tup.edu.ph>
2025-04-22 22:27:38 +0800

    Add 'Actions' column to customer table with edit buttons

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ if ($conn){
                 <th>Contact Number</th>
                 <th>Isle Number</th>
                 <th>Shelf Number</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -48,6 +49,7 @@ if ($conn){
         echo "<td>" .  htmlspecialchars($row['contact_number']). "</td>";
         echo "<td>" .  htmlspecialchars($row['isle_number']) . "</td>";
         echo "<td>" .  htmlspecialchars($row['shelf']). "</td>";
+        echo "<td> <button>Edit</button><button>Edit</button> </td>";
       }
     }else {
       echo "<tr> <td colspan = '6'>No data found </td> </tr>";
