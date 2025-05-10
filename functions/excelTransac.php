fbc2b402b7cd2a7c268a6ae8c4ec5c1fa325c21c
MjComia <markjoel.comia@tup.edu.ph>
2025-05-10 16:42:24 +0800

    Add export to Excel functionality for transactions and update transaction page layout

---
+++ functions/excelTransac.php
@@ -0,0 +1,46 @@
+<?php
+$host = 'localhost';
+$username =  'root';
+$password = '';
+$database = 'new-ims';
+$conn = ''; 
+
+try{
+  $conn = new mysqli($host, $username, $password, $database);
+
+}catch(mysqli_sql_exception){
+  echo"Could not connect to the database";
+}
+if ($conn){
+  // echo "Connected";
+}
+
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="transac_data.csv"');
+
+// Open output stream
+$output = fopen("php://output", "w");
+
+// Write CSV column headers
+fputcsv($output, ['Transaction ID', 'Customer ID', 'Product ID', 'Quantity', 'Total Price', 'Purchase Date']);
+
+
+$query = "SELECT * FROM transactions_table";
+$result = $conn->query($query);
+// Write each row to CSV
+while ($row = $result->fetch_assoc()) {
+    fputcsv($output, [
+        $row['transaction_id'],
+        $row['customer_id'],
+        $row['product_id'],
+        $row['quantity'],
+        $row['total_price'],
+        $row['purchase_date']
+    ]);
+}
+
+
+fclose($output);
+$conn->close();
+exit;
+?>
--- transaction.php
+++ transaction.php
@@ -24,6 +24,22 @@ if ($conn){
 <div class="card border-2">
   <div class="card-body">
     <div class = "card-header">Inventory</div>
+        <div class="row">
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1"></div>
+      <div class="col-1">
+        <a href = "functions/excelTransac.php" class = "btn btn-success"><i class = "dwn">Export to Excel</i></a>
+      </div>      
+    </div>
 <table id="myTable" class="display">
         <thead>
             <tr>
